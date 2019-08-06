eaf21e3f631a501d9c402974fa33e66972fadac8
e.vladisenko <e.vladisenko@gmail.com>
2019-08-06 19:12:31 +0300

    fix issue with sending zero length data frame

--- sw/src/main.cpp
+++ sw/src/main.cpp
@@ -149,7 +149,7 @@ CANbus::Status SendCANMsg (uint8_t type)
     else if (i < 9)                       // get DLC
     {
       msg.DLC = char_to_hex(tmp);
-      if (msg.RTR) break;                 // if this is remote frame no need send data
+      if ((msg.RTR) || (msg.DLC == 0)) break; // if this is remote frame or zero data length, no need to send data
     }
     else                                  // get DATA
     {
